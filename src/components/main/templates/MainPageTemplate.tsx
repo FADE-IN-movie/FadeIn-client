3dc5ea1d5dccf95235edb50da24c597f0cbc9d2e
김지수 <jisudev00@gmail.com>
2022-10-21 16:39:56 +0900

    Add: 메인 이미지 추가

---
+++ src/components/main/templates/MainPageTemplate.tsx
@@ -0,0 +1,13 @@
+import SearchBar from "@components/common/SearchBar";
+import BackgroundImg from "../molecules/BackgroundImg";
+
+function MainPageTemplate() {
+  return (
+    <div>
+      <BackgroundImg />
+      {/* <SearchBar main width="43rem" /> */}
+    </div>
+  );
+}
+
+export default MainPageTemplate;
