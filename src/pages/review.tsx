248f2a8bd35efa06eec1edd6dd72a91745cac743
김지수 <jisudev00@gmail.com>
2022-10-25 09:28:39 +0900

    Feat: SEO 구현

--- src/pages/review.tsx
+++ src/pages/review.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
+import SEO from "@components/common/SEO";
 import Calendar from "@components/review/organisms/Calendar";
 
 function ReviewPage() {
   return (
     <Wrap>
+      <SEO title="review" />
       <Calendar />
     </Wrap>
   );
