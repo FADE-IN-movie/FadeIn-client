62b3f0789e0a994ae9aec4f613e94231735c9fd7
김지수 <jisudev00@gmail.com>
2022-11-01 17:27:45 +0900

    Feat: Search keyword SEO 적용

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,14 @@
+import { useRouter } from "next/router";
 import styled from "styled-components";
 
+import SEO from "@components/common/SEO";
 import SearchPageTemplate from "@components/search/templates/SearchPageTemplate";
 
 function SearchPage() {
+  const { query } = useRouter();
   return (
     <Wrap>
+      <SEO title={query.keyword as string} />
       <SearchPageTemplate />
     </Wrap>
   );
