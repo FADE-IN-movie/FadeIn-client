4699061d0b807879d395f70c7ffae36efcda9134
김지수 <jisudev00@gmail.com>
2022-11-03 14:56:27 +0900

    Feat: type별 list title 적용

--- src/components/main/organisms/ListSection.tsx
+++ src/components/main/organisms/ListSection.tsx
@@ -2,20 +2,34 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import { useRecoilValue } from "recoil";
-import { recommendContentsQuery } from "@states/contents";
+import { recommendContentsQuery, selectedTypeState } from "@states/contents";
+import { loggedUserState } from "@states/users";
 
 import contents from "@lib/api/contentsAPI";
-
 import { IContentInfo } from "@typings/info";
 
 import CustomTitle from "@components/common/CustomTitle";
 import ContentCard from "@components/common/ContentCard";
 import Carousel from "@components/common/Carousel";
-import { Suspense } from "react";
+
+type ListTitlesType = {
+  [title: string]: string;
+};
 
 function ListSection() {
   const [content, setContent] = useState<any>({});
+  const type =
+    useRecoilValue(selectedTypeState) === "movie" ? "영화" : "TV 프로그램";
+  const { userName } = useRecoilValue(loggedUserState);
   // const contents = useRecoilValue(recommendContentsQuery);
+
+  const listTitles: ListTitlesType = {
+    popular: `실시간 ${type} Top 10`,
+    topRate: `역대 ${type} Top`,
+    nowPlaying: `현재 상영 중인 ${type}`,
+    preference: `${userName}님이 좋아할만한 ${type}`,
+    recommend: `FADE-IN 추천 ${type}`,
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -31,7 +45,7 @@ function ListSection() {
       {Object.keys(content)?.map((title, i) => (
         <div key={i}>
           <TitleWrap>
-            <CustomTitle>{title}</CustomTitle>
+            <CustomTitle>{listTitles[title]}</CustomTitle>
           </TitleWrap>
           <Carousel>
             {content[title]?.map((info: IContentInfo, i: number) => (
