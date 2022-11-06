66d95dd1f29e03fa934099da3d22ee45f31f02f3
Jisu00 <jisudev00@gmail.com>
2022-11-06 18:56:13 +0900

    Feat: narrow Grid 추가 및 비슷한 컨텐츠에 적용

--- src/components/layouts/Grid.tsx
+++ src/components/layouts/Grid.tsx
@@ -1,17 +1,18 @@
-import React from "react";
 import { useEffect, useState, useRef } from "react";
 import { ReactNode } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface IProps {
   children: ReactNode;
+  narrow?: boolean;
 }
 
 type LayoutPropsType = {
   width: number;
+  narrow?: boolean;
 };
 
-function Grid({ children }: IProps) {
+function Grid({ children, narrow }: IProps) {
   const [width, setWidth] = useState(0);
   const layoutRef = useRef(null);
 
@@ -33,7 +34,7 @@ function Grid({ children }: IProps) {
   }, []);
 
   return (
-    <Layout ref={layoutRef} width={width}>
+    <Layout ref={layoutRef} narrow={narrow} width={width}>
       {children}
     </Layout>
   );
@@ -46,4 +47,60 @@ const Layout = styled.div<LayoutPropsType>`
   flex-wrap: wrap;
   gap: 5rem 1rem;
   font-size: ${(props) => `${props.width}px`};
+
+  & > div {
+    @media screen and (min-width: 1651px) {
+      width: calc(12.5% - 0.9rem);
+    }
+    @media screen and (max-width: 1650px) {
+      width: calc(14.25% - 0.85rem);
+    }
+    @media screen and (max-width: 1400px) {
+      width: calc(16.67% - 0.85rem);
+    }
+    @media screen and (max-width: 1280px) {
+      width: calc(20% - 0.85rem);
+    }
+    @media screen and (max-width: 950px) {
+      width: calc(25% - 0.85rem);
+    }
+    @media screen and (max-width: 720px) {
+      width: calc(33.3% - 0.85rem);
+    }
+    @media screen and (max-width: 500px) {
+      width: calc(50% - 0.85rem);
+    }
+    @media screen and (max-width: 300px) {
+      width: 100%;
+    }
+
+    ${(props) =>
+      props.narrow &&
+      css`
+        @media screen and (min-width: 1280px) {
+          width: calc(20% - 0.85rem);
+        }
+        @media screen and (max-width: 1279px) {
+          width: calc(14.25% - 0.85rem);
+        }
+        @media screen and (max-width: 1100px) {
+          width: calc(16.67% - 0.85rem);
+        }
+        @media screen and (max-width: 950px) {
+          width: calc(20% - 0.85rem);
+        }
+        @media screen and (max-width: 720px) {
+          width: calc(25% - 0.85rem);
+        }
+        @media screen and (max-width: 600px) {
+          width: calc(33.3% - 0.85rem);
+        }
+        @media screen and (max-width: 500px) {
+          width: calc(50% - 0.85rem);
+        }
+        @media screen and (max-width: 300px) {
+          width: 100%;
+        }
+      `}
+  }
 `;
