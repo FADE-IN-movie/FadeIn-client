3d0d48f64eb845f400fcc851a94b2767895e6cdc
김지수 <jisudev00@gmail.com>
2022-11-25 17:11:39 +0900

    Feat: Search 결과 개수 보여주는 기능 구현 완료

--- src/components/search/atoms/ResultText.tsx
+++ src/components/search/atoms/ResultText.tsx
@@ -2,17 +2,16 @@ import { useRouter } from "next/router";
 import styled from "styled-components";
 
 import { useRecoilValueLoadable } from "recoil";
-import { searchKeywordQuery } from "@states/search";
+import { searchResultCntQuery } from "@states/search";
 
 const ResultText = () => {
-  const { state, contents } = useRecoilValueLoadable(searchKeywordQuery);
+  const { contents: resultCnt } = useRecoilValueLoadable(searchResultCntQuery);
   const { query } = useRouter();
 
   return (
     <Text>
       <span className="bold">&apos;{query?.keyword}&apos; </span>검색 결과가{" "}
-      {!contents || state === "loading" ? 0 : contents.search?.length}개
-      있습니다.
+      {resultCnt?.total}개 있습니다.
     </Text>
   );
 };
