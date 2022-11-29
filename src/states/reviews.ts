f46415d32a439f1a55f30fe2cae94ce8cf708e4a
김지수 <jisudev00@gmail.com>
2022-11-29 15:36:23 +0900

    Feat: Calendar 선택 기능 추가

--- src/states/reviews.ts
+++ src/states/reviews.ts
@@ -1,5 +1,6 @@
 import { atom } from "recoil";
 import { IReviewInfo } from "@typings/info";
+import { IDate } from "@typings/date";
 
 export const reviewDetailState = atom<IReviewInfo>({
   key: "reviewDetailState",
@@ -12,5 +13,23 @@ export const reviewDetailState = atom<IReviewInfo>({
     review: {
       rating: 0,
     },
+  },
+});
+
+export const todayDateState = atom<IDate>({
+  key: "todayDateState",
+  default: {
+    year: 0,
+    month: 0,
+    date: 0,
+  },
+});
+
+export const selectedDateState = atom<IDate>({
+  key: "selectedDateState",
+  default: {
+    year: 0,
+    month: 0,
+    date: 0,
   },
 });
