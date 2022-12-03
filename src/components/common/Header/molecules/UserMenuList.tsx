73ee742b13d0e6509208aede3055b41b218ac699
Jisu00 <jisudev00@gmail.com>
2022-12-03 19:49:36 +0900

    Feat: User Menu List에서 내 감상평 추가 및 페이지 이동 처리 완료

--- src/components/common/Header/molecules/UserMenuList.tsx
+++ src/components/common/Header/molecules/UserMenuList.tsx
@@ -1,13 +1,18 @@
+import { useRouter } from "next/router";
 import styled from "styled-components";
 import { signOut } from "@utils/account";
 
 import UserMenu from "../atoms/UserMenu";
 
 const UserMenuList = () => {
+  const router = useRouter();
+
   return (
     <List>
       <UserMenu>찜 목록</UserMenu>
-      <UserMenu>내가 ... 기록</UserMenu>
+      <UserMenu handleClick={() => router.push("/review")}>
+        내가 감상평
+      </UserMenu>
       <UserMenu handleClick={signOut}>로그아웃</UserMenu>
     </List>
   );
