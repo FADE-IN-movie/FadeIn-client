2bcf14b17e9c4602bea58239b9e4c21555f9db1e
김지수 <jisudev00@gmail.com>
2022-12-13 09:55:26 +0900

    Feat: 404 SEO 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,16 @@
+import styled from "styled-components";
 import NotFoundTemplate from "@components/404/templates/NotFoundTemplate";
+import SEO from "@components/common/SEO";
 
 const NotFound = () => {
-  return <NotFoundTemplate />;
+  return (
+    <Wrap>
+      <SEO title="Not Found" />
+      <NotFoundTemplate />
+    </Wrap>
+  );
 };
 
 export default NotFound;
+
+const Wrap = styled.div``;
