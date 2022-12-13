9eb879c52a2b00f8c17ad8135292d9b84e826234
김지수 <jisudev00@gmail.com>
2022-12-13 09:44:02 +0900

    Feat: 내 감상평 SEO 추가

--- src/pages/mylist.tsx
+++ src/pages/mylist.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 import MyListTemplate from "@components/mylist/templates/MyListTemplate";
+import SEO from "@components/common/SEO";
 
 const MyListPage = () => {
   return (
     <Wrap>
+      <SEO title="내 감상평" />
       <MyListTemplate />
     </Wrap>
   );
