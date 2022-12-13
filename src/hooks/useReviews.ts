14acf189a0e44846baf660a789f0775a7f50c411
김지수 <jisudev00@gmail.com>
2022-12-13 10:15:36 +0900

    Feat: 리뷰 삭제 성공/실패 시 알림 기능 추가

--- src/hooks/useReviews.ts
+++ src/hooks/useReviews.ts
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 import useSWR from "swr";
 import reviews from "@lib/api/reviewsAPI";
 
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { successAlertState, errorAlertState } from "@states/alert";
 import { selectedDateState } from "@states/reviews";
 import { IReviewInfo } from "@typings/info";
 
@@ -13,6 +14,8 @@ const useReviews = () => {
     year && month ? ["reviews", year, month] : null,
     () => reviews.getReviews(year, month)
   );
+  const setSuccessAlert = useSetRecoilState(successAlertState);
+  const setErrorAlert = useSetRecoilState(errorAlertState);
 
   const onDeleteReview = (reviewId: string) => {
     if (reviewId === "") return;
@@ -25,9 +28,10 @@ const useReviews = () => {
         );
 
         mutate({ review: newData }, false);
+        setSuccessAlert("리뷰 삭제가 완료되었습니다.");
       })
-      .catch((err) => {
-        console.dir(err);
+      .catch(() => {
+        setErrorAlert("리뷰를 삭제하지 못했습니다. 다시 시도해주세요.");
       });
   };
 
