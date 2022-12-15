8a69b96d9ae09216c400ca9f8ce973bba69a586a
김지수 <jisudev00@gmail.com>
2022-12-15 16:09:19 +0900

    Feat: 무한 스크롤 구현

--- src/components/rank/organisms/RankItems.tsx
+++ src/components/rank/organisms/RankItems.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import { theme } from "@styles/theme";
 
@@ -7,16 +8,46 @@ import useRank from "@hooks/useRank";
 import { IContentInfo } from "@typings/info";
 
 const RankItems = () => {
-  const { ranking, isLoading } = useRank();
+  const [target, setTarget] = useState<HTMLElement | null | undefined>(null);
+  const { ranking, isValidating, isSameSize, setSize } = useRank();
 
-  if (isLoading) return null;
-  if (!ranking.length) return <Text>( 해당 정보가 존재하지 않습니다. )</Text>;
+  const onIntersect: IntersectionObserverCallback = useCallback(
+    ([entry], observer) => {
+      if (entry.isIntersecting && !isValidating && isSameSize) {
+        observer.unobserve(entry.target);
+        setSize((prev) => prev + 1);
+        observer.observe(entry.target);
+      }
+    },
+    [isValidating, isSameSize, setSize]
+  );
+
+  useEffect(() => {
+    if (!target) return;
+
+    const observer: IntersectionObserver = new IntersectionObserver(
+      onIntersect,
+      {
+        threshold: 0.7,
+      }
+    );
+    observer.observe(target);
+
+    return () => observer && observer.disconnect();
+  }, [target, onIntersect]);
+
+  if (ranking === undefined) return null;
+  if (ranking && !ranking.length)
+    return <Text>( 해당 정보가 존재하지 않습니다. )</Text>;
   return (
-    <Grid>
-      {ranking?.map((result: IContentInfo, i: number) => (
-        <ContentCard key={i} responsive info={result} />
-      ))}
-    </Grid>
+    <div>
+      <Grid>
+        {ranking?.map((result: IContentInfo, i: number) => (
+          <ContentCard key={i} responsive info={result} />
+        ))}
+      </Grid>
+      <div ref={setTarget} style={{ marginTop: "10rem" }} />
+    </div>
   );
 };
 
