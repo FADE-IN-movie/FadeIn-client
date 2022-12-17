ee48664410f0fc52b5d88611dcede5af33717768
Jisu00 <jisudev00@gmail.com>
2022-12-17 19:36:44 +0900

    Feat: SearchItems Skeleton 추가

---
+++ src/components/search/organisms/SearchItemsSkeleton.tsx
@@ -0,0 +1,27 @@
+import Grid from "@components/layouts/Grid";
+import ContentCardSkeleton from "@components/common/ContentCard/Skeleton";
+
+const SearchItemsSkeleton = () => {
+  return (
+    <Grid>
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+      <ContentCardSkeleton />
+    </Grid>
+  );
+};
+
+export default SearchItemsSkeleton;
